285f96e1b5a30430b8e1923b9c715e33d310b5c7
Baze Petrushev <b.petrushev@gmail.com>
2015-12-21 22:37:26 +0100

    Minor refactoring

--- txplayagui/library.py
+++ txplayagui/library.py
@@ -2,47 +2,10 @@ import gc
 
 from PyQt5.QtCore import QAbstractItemModel, QModelIndex, Qt, pyqtSignal
 
-from txplayagui.utilities import mimeWrapJson
+from txplayagui.utilities import mimeWrapJson, SortedDict
 
 
 # TODO : split into separate widget
-class SortedDict(dict):
-
-    def __init__(self, *args, **kwargs):
-        dict.__init__(self, *args, **kwargs)
-        self._orderedKeys = sorted(self.keys())
-
-    def __setitem__(self, key, val):
-        resort = (key not in self)
-        dict.__setitem__(self, key, val)
-        if resort:
-            self._orderedKeys = sorted(self.keys())
-
-    def __delitem__(self, key):
-        dict.__delitem__(self, key)
-        self._orderedKeys.remove(key)
-
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            key = self._orderedKeys[key]
-        return dict.__getitem__(self, key)
-
-    def itemAt(self, index):
-        key = self._orderedKeys[index]
-        return (key, self[key])
-
-    def index(self, key):
-        return self._orderedKeys.index(key)
-
-    def clear(self, *args, **kwargs):
-        dict.clear(self, *args, **kwargs)
-        self._orderedKeys[:] = []
-
-    def popitem(self):
-        key, val = dict.popitem(self)
-        self._orderedKeys.remove(key)
-        return key, val
-
 
 class LibraryItem(object):
 
--- txplayagui/utilities.py
+++ txplayagui/utilities.py
@@ -11,3 +11,41 @@ def mimeWrapJson(data):
 
 def unwrapMime(mimeData):
     return json.loads(mimeData.text())
+
+
+class SortedDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self._orderedKeys = sorted(self.keys())
+
+    def __setitem__(self, key, val):
+        resort = (key not in self)
+        dict.__setitem__(self, key, val)
+        if resort:
+            self._orderedKeys = sorted(self.keys())
+
+    def __delitem__(self, key):
+        dict.__delitem__(self, key)
+        self._orderedKeys.remove(key)
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            key = self._orderedKeys[key]
+        return dict.__getitem__(self, key)
+
+    def itemAt(self, index):
+        key = self._orderedKeys[index]
+        return (key, self[key])
+
+    def index(self, key):
+        return self._orderedKeys.index(key)
+
+    def clear(self, *args, **kwargs):
+        dict.clear(self, *args, **kwargs)
+        self._orderedKeys[:] = []
+
+    def popitem(self):
+        key, val = dict.popitem(self)
+        self._orderedKeys.remove(key)
+        return key, val
