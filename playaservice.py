31033e433b995c8461d1e12c522b892370db64e6
Baze Petrushev <b.petrushev@gmail.com>
2015-12-23 02:14:22 +0100

    Added configurability of bind address and port

--- playaservice.py
+++ playaservice.py
@@ -1,9 +1,14 @@
+from os import environ
+
 from twisted.application.service import Application
 from twisted.application.internet import TCPServer
 
 from txplaya.http import HttpFactory
 
+BIND_ADDRESS = environ.get('TXPLAYA_BIND_ADDRESS', 'localhost')
+PORT = int(environ.get('TXPLAYA_PORT', 8070))
+
 httpFactory = HttpFactory()
 
 application = Application('Playa')
-TCPServer(8070, httpFactory).setServiceParent(application)
+TCPServer(PORT, httpFactory, interface=BIND_ADDRESS).setServiceParent(application)
